f98e7c77cca5b29d950413d1578d66d699301b03
Ionara <235911627+IonaraParreira@users.noreply.github.com>
2026-06-18 20:59:59 -0300

    Update regex to include additional special character

--- senha_validada.py
+++ senha_validada.py
@@ -8,12 +8,12 @@ senha = input("A senha tem que ter (mínimo 8 caracteres, incluindo: maiúscula,
 # 2. A nova Regex explicada por partes:
 # ^             -> Início da string
 # (?=.*\d)      -> SENSOR 1: Obriga a ter pelo menos um NÚMERO
-# (?=.*[@$!%*?&]) -> SENSOR 2: Obriga a ter pelo menos um CARACTERE ESPECIAL
+# (?=.*[@$!%*?&#]) -> SENSOR 2: Obriga a ter pelo menos um CARACTERE ESPECIAL
 # (?=.*[A-Z])   -> SENSOR 3: Obriga a ter pelo menos uma letra MAIÚSCULA
 # (?=.*[a-z])   -> SENSOR 4: Obriga a ter pelo menos uma letra MINÚSCULA
 # .{8,}         -> REGRA 5: Mantém a exigência de ter no mínimo 8 caracteres no total
 # $             -> Fim da string
-padrao_seguro = r"^(?=.*\d)(?=.*[@$!%*?&])(?=.*[A-Z])(?=.*[a-z]).{8,}$"
+padrao_seguro = r"^(?=.*\d)(?=.*[@$!%*?&#])(?=.*[A-Z])(?=.*[a-z]).{8,}$"
 
 # 3. Faz a validação
 if re.fullmatch(padrao_seguro, senha):
